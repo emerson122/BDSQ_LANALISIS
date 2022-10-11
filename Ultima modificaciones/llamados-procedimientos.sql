46d08138e9a77bd3370994f74adbf457f03f1892
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-10-11 00:35:22 -0600

    modified:   Ultima modificaciones/PROCEDIMIENTOS.SQL 	modified:   Ultima modificaciones/llamados-procedimientos.sql

--- Ultima modificaciones/llamados-procedimientos.sql
+++ Ultima modificaciones/llamados-procedimientos.sql
@@ -1,3 +1,4 @@
+/* ------------------------------------------------------------------ */
 /* iNSERTAR PERSONAS*/
 call PRC_PERSONAS('elias vasquez', 'm', 21, 'j', '080129912132', 's', '1', 3341043, 'c', 1, 1);
 call PRC_PERSONAS('Victor mendez', 'm', 21, 'j', '080129912132', 's', '1', 3341043, 'c', 1, 1);
@@ -29,7 +30,7 @@ call PRC_CLASIFICACIONES('', 4, '');
 /* Seleccionar una especifica */
 call PRC_CLASIFICACIONES('', 5, 1);
 
-
+/* ------------------------------------------------------------------ */
 
 /*Insertar cuenta generando Catalago de cuenta*/
 call INS_CUENTAS('activo', 'bancos', 1);
@@ -44,7 +45,7 @@ call PRC_CUENTAS('', '', '', 3, 2);
 call PRC_CUENTAS('', '', '', 4, '');
 /*Seleccionar una Cuenta */
 call PRC_CUENTAS('', '', '', 5, 1);
-
+/* ------------------------------------------------------------------ */
 
 /* insertar subcuenta */
 CALL PRC_SUBCUENTAS(1, '1', 'EFECTIVO', 'BANCOS', 1, 1);
@@ -61,7 +62,7 @@ CALL PRC_SUBCUENTAS('', '', '', '', 4, '');
 /* Selecciona una subcuenta especifica */
 CALL PRC_SUBCUENTAS('', '', '', '', 5, 2);
 
-
+/* ------------------------------------------------------------------ */
 /*Insertar roles*/
 CALL PRC_MS_ROLES( 'ADMINISTRADOR', 'USUARIO CON TODOS LOS PRIVILEGIOS', 1, '');
 CALL PRC_MS_ROLES( 'usuario', 'USUARIO ESTANDAR', 1, '');
@@ -77,19 +78,19 @@ CALL PRC_MS_ROLES( '', '', 4, '');
 /* Seleccion especifica */
 CALL PRC_MS_ROLES( '', '', 5, 1);
 
-
+/* ------------------------------------------------------------------ */
 
 /*iNSERTAR OBJETO */
 CALL PRC_OBJETOS('HOME', 'PANTALLA PRINCIPAL', 'PANTALLA', 1, '');
 
 /*SELECCIONAR OBJETOS */
 CALL PRC_OBJETOS('HOME', 'PANTALLA PRINCIPAL', 'PANTALLA', 4, '');
-
+/* ------------------------------------------------------------------ */
 /*INSERTAR Usuario CON CORREO PREGUNTAS Y RESPUESTAS*/
 CALL PROC_MS_USR_INSERTAR('USUARIO1', 'HERNESTO VALLADARES', 1, '2022-02-02', 1, 1, 'HEVA@gmail.com', 'Cual es tu color favorito?','verde' , 'secreta1');
 /*SELECCIONAR USUARIOS */
 CALL PROC_MS_USR_SELECCIONAR();
-
+/* ------------------------------------------------------------------ */
 
 /* INSERTAR PERIODO */
 CALL PRC_PERIODOS(1, 'PERIODO-ENE-2022-001', '2022-01-01', '2022-02-01', 'ACTIVO', 1, 1);
@@ -101,7 +102,7 @@ CALL PRC_PERIODOS('', '', '', '', '', 3, 1);
 /*SELECCIONAR PERIODO */
 CALL PRC_PERIODOS('', '', '', '', '', 4, '');
 
-
+/* ------------------------------------------------------------------ */
 /* INSERTAR CUENTA CON SUBCUENTA EN LIBRO DIARIO */
 CALL PRC_LIBDIARIO(1,'bancos', 'EFECTIVO', 2000, 0, 1, 1);
 
@@ -109,12 +110,27 @@ CALL PRC_LIBDIARIO(1,'bancos', 'EFECTIVO', 2000, 0, 1, 1);
 /* INSERTAR DATOS SOLO DE CUENTA SIN SUBCUENTA */
 CALL PRC_LIBDIARIO(1,'bancos', '', 2000, 0, 2, 1);
 
+/* ------------------------------------------------------------------ */
 
-/* INSERTAR EN LIBRO MAYOR */
-CALL PRC_LIBROS_MAYORES(1, 'bancos', 1, '');
+/* INSERTAR EN LIBRO MAYOR CUENTAS DE LIBRO DIARIO */
+CALL PRC_LIBROS_MAYORES(1, 'BANCOS', 0, 0, 1, 1);
+
+/* INSERTAR EN LIBRO MAYOR CUENTAS QUE NO ESTAN EN LIBRO DIARIO*/
+CALL PRC_LIBROS_MAYORES(1, 'BANCOS', 100, 0, 6, 1);
+
+/* Actualizar libro MAYOR */
+CALL PRC_LIBROS_MAYORES(1, 'BANCOS', 100, 0, 2, 1);
+
+/* ELIMINAR LIBRO MAYOR */
+CALL PRC_LIBROS_MAYORES('', '', '', '', 3, 1);
+/* SELECCIONAR LIBRO MAYOR */
+CALL PRC_LIBROS_MAYORES('', '', '', '', 4,'');
+/* SELECCIONAR SOLO 1 */
+CALL PRC_LIBROS_MAYORES('', '', '', '', 5,2);
 
 
-
+/* ------------------------------------------------------------------ */
+/* INSERTAR EN EL BALANCE GENERAL */
 /* BALANCE GENERAL ACTIVOS */
 CALL INS_BAL_GENERAL('HTOURS', 'ACTIVO', 1);
 /* BALANCE GENERAL PASIVOS */
