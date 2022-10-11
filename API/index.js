5ada99e07da54a6595569b126949054c4a9ffbd3
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-10-11 16:54:57 -0600

    new file:   .gitignore 	new file:   API/index.js 	new file:   API/package-lock.json 	new file:   API/package.json 	modified:   Ultima modificaciones/PROCEDIMIENTOS.SQL

---
+++ API/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+
+const conexion = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "systemhtours",
+    multipleStatements: true,
+})
+
+// Para verificar si la conexión es exitosa para Falló mientras se ejecuta el proyecto en la consola.
+conexion.connect((err) => {
+    if (!err) {
+      console.log("Conexión Exitosa A la base de datos");
+    } else {
+      console.log(
+        "Conexión fallida en la base de datos twot \n Error :" + JSON.stringify(err, undefined, 2)
+      );
+    }
+  });
+
+app.set('port',process.env.PORT || 3000);
+
+app.get('/',(req,res)=>{
+    res.send('HOLA MUNDO')
+})
+
+// leer 
+app.get(["/balance", "/Leer"],(req, res)=>{
+    const sql = `call INS_BAL_GENERAL('', 1, '', 5)`
+    conexion.query(sql,(error,results)=>{
+        if(error) throw error;
+        if(results.length>0){
+            res.json(results[0]);
+        }else{
+            res.send('No se pudo obtener resultados')
+        }
+    });  
+    console.log('Datos leidos correctamente');
+});
+
+
+app.listen(app.set('port'),()=>{
+    console.log('servidor encendido');
+})
