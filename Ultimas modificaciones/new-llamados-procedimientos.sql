8fa5dc07a9f5e3c88934326c41fa540610897b3d
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-11-14 11:54:59 -0600

    modified:   Ultimas modificaciones/PROCEDIMIENTOS.SQL 	modified:   Ultimas modificaciones/new-llamados-procedimientos.sql

--- Ultimas modificaciones/new-llamados-procedimientos.sql
+++ Ultimas modificaciones/new-llamados-procedimientos.sql
@@ -7,13 +7,18 @@ call PRC_MS_INSERT_ROLES('Administrador', 'Control Absoluto del sistema');
 call PRC_MS_INSERT_ROLES('DEFAULT', 'Rol por defecto con el que se crea un usuario');
 
 -- NO UTILIZAR EN PRODUCCION el ROL DEVS, Colocarse este Rol para poder acceder al sistema o el rol administrador
-call PRC_MS_INSERT_ROLES('DEVS', 'USUARIO CODIFICADOR'); 
+call PRC_MS_INSERT_ROLES('MANTENIMIENTO', 'USUARIO CODIFICADOR'); 
 /*Ingresar un Objeto*/
 CALL PRC_OBJETOS('HOME', 'PANTALLA PRINCIPAL', 'PANTALLA', 1, '');
+CALL PRC_OBJETOS('LOGIN', 'PANTALLA DE INICIO DE SESION', 'PANTALLA', 1, '');
+CALL PRC_OBJETOS('REGISTRO', 'PANTALLA DE REGISTRO DE USUARIO EXTERNA', 'PANTALLA', 1, '');
 /*Ingresar un Usuario Con control absoluto*/
-CALL PROC_MS_USR_INSERTAR('root','TecnoBot ', 1, '2022-02-02', 1, 1, 'tecnobot@gmail.com', 'Cual es tu color favorito?','verde' , ' 0a1b7c9440e4afe5e31ea6a190490bd3');
+CALL PROC_MS_USR_INSERTAR('ROOT','TecnoBot ', 1, '2022-02-02', 1, 1, 'tecnobot@gmail.com', 'Cual es tu color favorito?','verde' , ' 0a1b7c9440e4afe5e31ea6a190490bd3');
+CALL PROC_MS_USR_INSERTAR('MANT','TecnoBot Mantenimiento', 3, '2022-02-02', 1, 1, 'tecnobot@gmail.com', 'Cual es el mantenimiento Favorito?','programacion' , '137772b7ecb263dc707ab445c56c0181');
+CALL PROC_MS_USR_INSERTAR('Usuario_Auto_Registrado','Usuario Autoregisro Externo', 3, '2022-02-02', 1, 1, 'tecnobot@gmail.com', 'Cual es el registro Favorito?','registro' , '137772b7ecb263dc707ab445c56c0181');
 /*
- contrasena de este usuario :   vSupnqtYUW21   */
+ contrasena de  usuario  ROOT:   vSupnqtYUW21   */
+ -- VTnodJbt0Sxqd5OhWYGTZwcw5Ma
  
  /*Parametros del sistema */
  
