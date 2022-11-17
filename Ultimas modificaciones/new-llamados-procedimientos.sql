bd3a5fe5a1ed944676cddbe567c02f5a5db14da8
Exequiel Velaquez <75557699+emerson122@users.noreply.github.com>
2022-11-16 18:14:53 -0600

    integracion de objeto

--- Ultimas modificaciones/new-llamados-procedimientos.sql
+++ Ultimas modificaciones/new-llamados-procedimientos.sql
@@ -7,11 +7,17 @@ call PRC_MS_INSERT_ROLES('Administrador', 'Control Absoluto del sistema');
 call PRC_MS_INSERT_ROLES('DEFAULT', 'Rol por defecto con el que se crea un usuario');
 
 -- NO UTILIZAR EN PRODUCCION el ROL DEVS, Colocarse este Rol para poder acceder al sistema o el rol administrador
-call PRC_MS_INSERT_ROLES('MANTENIMIENTO', 'USUARIO CODIFICADOR'); 
+call PRC_MS_INSERT_ROLES('MANTENIMIENTO', 'USUARIO CODIFICADOR');
+
+
 /*Ingresar un Objeto*/
 CALL PRC_OBJETOS('HOME', 'PANTALLA PRINCIPAL', 'PANTALLA', 1, '');
 CALL PRC_OBJETOS('LOGIN', 'PANTALLA DE INICIO DE SESION', 'PANTALLA', 1, '');
 CALL PRC_OBJETOS('REGISTRO', 'PANTALLA DE REGISTRO DE USUARIO EXTERNA', 'PANTALLA', 1, '');
+CALL PRC_OBJETOS('PERSONAS', 'PANTALLA DE GESTION DE PERSONAS', 'PANTALLA', 1, '');
+CALL PRC_OBJETOS('PERIODO', 'PANTALLA DE GESTION DE PERIODO', 'PANTALLA', 1, '');
+
+
 /*Ingresar un Usuario Con control absoluto*/
 CALL PROC_MS_USR_INSERTAR('ROOT','TecnoBot ', 1, '2022-02-02', 1, 1, 'tecnobot@gmail.com', 'Cual es tu color favorito?','verde' , ' 0a1b7c9440e4afe5e31ea6a190490bd3');
 CALL PROC_MS_USR_INSERTAR('MANT','TecnoBot Mantenimiento', 3, '2022-02-02', 1, 1, 'tecnobot@gmail.com', 'Cual es el mantenimiento Favorito?','programacion' , '137772b7ecb263dc707ab445c56c0181');
